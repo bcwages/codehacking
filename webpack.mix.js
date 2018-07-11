8ef3e2f1655d1bc50448e4873e91307b849e0ea8
WAGES <bw109m@att.com>
2018-07-11 17:22:46 -0500

    User create working wihtout photos

--- webpack.mix.js
+++ webpack.mix.js
@@ -25,10 +25,10 @@ mix.styles([
 ], 'public/css/libs.css');
 
 mix.scripts([
+    'resources/assets/js/libs/jquery.js',
     'resources/assets/js/libs/bootstrap.js',
     'resources/assets/js/libs/metisMenu.js',
     'resources/assets/js/libs/sb-admin-2.js',
-    'resources/assets/js/libs/jquery.js',
     'resources/assets/js/libs/scripts.js'
 
 ], 'public/js/libs.js');
