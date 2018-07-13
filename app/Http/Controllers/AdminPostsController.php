31ab7210de5293c25880673214794ff9f1bac406
WAGES <bw109m@att.com>
2018-07-12 19:37:38 -0500

    Added the category process

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use App\Http\Requests\PostsCreateRequest;
 use App\Photo;
 use App\Post;
@@ -19,7 +20,7 @@ class AdminPostsController extends Controller
     {
         //
         $posts = Post::all();
-        return view('admin.posts.index', compact('posts'));
+        return view('admin.posts.index', compact('posts','categories'));
     }
 
     /**
@@ -30,7 +31,9 @@ class AdminPostsController extends Controller
     public function create()
     {
         //
-        return view('admin.posts.create');
+        $categories = Category::pluck('name','id')->all();
+
+        return view('admin.posts.create', compact('categories'));
     }
 
     /**
--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -13,8 +13,7 @@
 
             <div class="form-group">
                 {!! Form::label('category_id', 'Category:') !!}
-                {{--{!! Form::select('category_id', [''=>'Choose Categories'], null, ['class'=>'form-control'])!!}--}}
-                {!! Form::select('category_id', array(1=>'PHP', 2=>'Javascript'), null, ['class'=>'form-control'])!!}
+                {!! Form::select('category_id', [''=>'Choose Categories'] + $categories, null, ['class'=>'form-control'])!!}
             </div>
 
             <div class="form-group">
